99370cc4bd25d78a686d7992e704d367876d2f7e
Md Alamgir Kobir <makobirbd@gmail.com>
2024-10-06 14:33:23 +0600

    Create Web.php

---
+++ controller/Web.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Web extends CI_Controller {
+	 public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->load->library('bdbulk');
+        $this->load->library('session');
+        $this->load->library('porichoyapi');
+        $this->load->model('Web_model');
+        /* cache control */
+        $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $this->output->set_header('Pragma: no-cache');
+        $this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        
+        date_default_timezone_set('Asia/Dhaka');
+
+         $date = date('d-m-Y h:i:s A');
+        // echo '<br>'. $date;
+    }
+  
+    
+    public function index()
+    {  
+        
+        $nid_number = '599199XXXX';
+        $date_of_birth = '1990-10-30';
+
+            $result = $this->porichoyapi->verify_nid($nid_number, $date_of_birth, true);
+
+            // Check for errors
+            if (isset($result['error']) && $result['error'] === true) {
+                echo 'Error: ' . $result['message'];
+            } else {
+                $this->load->view('nid_verification_result', ['response_data' => $result]);
+            }
+
+    }      
+}
