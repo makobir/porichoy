26bb64655edceecbb0b98e6075a888797df8f1d5
Md Alamgir Kobir <makobirbd@gmail.com>
2024-10-06 14:37:48 +0600

    Update Web.php

--- controller/Web.php
+++ controller/Web.php
@@ -5,11 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Web extends CI_Controller {
 	 public function __construct() {
         parent::__construct();
-        $this->load->database();
-        $this->load->library('bdbulk');
-        $this->load->library('session');
         $this->load->library('porichoyapi');
-        $this->load->model('Web_model');
         /* cache control */
         $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
